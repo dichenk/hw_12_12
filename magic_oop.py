902acc5cfb8867d878c8ab2067f8acb9c5c66999
dichenk <dchenk@DESKTOP-R5OCG73.localdomain>
2022-12-12 23:14:50 +0300

    first task done

--- magic_oop.py
+++ magic_oop.py
@@ -1,1 +1,26 @@
-print('hello')
+from sympy import *
+import math
+
+class Derivative:
+    def __init__(self, func):
+        self.__func = func
+
+    def __call__(self, *args, **kwargs):
+        return diff(self.__func(x)).subs(x, args[0])
+
+# Просто считаем синус
+def sin_(x):
+    return sin(x)
+
+print(sin_(math.pi/3))
+#0.8660254037844386
+
+# Считаем производную
+@Derivative
+def sin__(x):
+    return sin(x)
+
+x = Symbol('x')
+
+print(sin__(math.pi/3))
+#0.4999566978958203
